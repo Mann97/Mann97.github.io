93701a705fe80d71cd4819764d15210332941855
Mann16 <98376935+Mann97@users.noreply.github.com>
2022-11-22 14:02:37 +0530

    bugs fixed

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "0c343528f734f257cfd3d459a6ae28e9",
-"index.html": "96e98fa301d2ff49bbb5122dcb992aef",
-"/": "96e98fa301d2ff49bbb5122dcb992aef",
+"index.html": "7f92395775d103dab83c82030db0010f",
+"/": "7f92395775d103dab83c82030db0010f",
 "main.dart.js": "12942cacf67702115dfcada088a8eed9",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
